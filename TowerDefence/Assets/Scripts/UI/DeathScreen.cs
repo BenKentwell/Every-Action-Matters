0421d29b4e0cf3f1d1cd1fadc4fcdb6b94823e23
lmcunneen <liam.cunneen282@gmail.com>
2024-03-05 21:18:15 +1100

    Created AudioMixer, Added Gameplay Music

--- TowerDefence/Assets/Scripts/UI/DeathScreen.cs
+++ TowerDefence/Assets/Scripts/UI/DeathScreen.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class DeathScreen : MonoBehaviour
 {
     public GameObject gameplayUI;
     public GameObject deathTab;
 
+    public AudioMixer musicMixer;
+
     void Start()
     {
         gameplayUI.SetActive(true);
         deathTab.SetActive(false);
+
+        musicMixer.SetFloat("MusicLowpass", 22000.00f);
     }
 
     public void OpenDeathScreen()
@@ -21,6 +26,8 @@ public class DeathScreen : MonoBehaviour
 
         gameplayUI.SetActive(false);
         deathTab.SetActive(true);
+
+        musicMixer.SetFloat("MusicLowpass", 600.00f);
     }
 
     public void RestartLevel()
