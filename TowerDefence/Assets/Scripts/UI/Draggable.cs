d30734f8e53c67aed3f09073d38cf5de377d3864
lmcunneen <liam.cunneen282@gmail.com>
2024-03-05 22:25:00 +1100

    Added Tower Placing SFX

--- TowerDefence/Assets/Scripts/UI/Draggable.cs
+++ TowerDefence/Assets/Scripts/UI/Draggable.cs
@@ -23,77 +23,84 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     private Transform originalParent;
 	public GameObject towerButton;
 
+	public AudioClip onDragSound;
+	public AudioClip onPlaceSound;
+	
 	private AudioSource audioSource;
 
 	private void Start()
 	{
 		GetComponent<Image>().sprite = spriteUI;
-		audioSource = GetComponent<AudioSource>();
+		audioSource = GameObject.FindWithTag("AudioManager").GetComponent<AudioSource>();
 	}
 
     public void OnBeginDrag(PointerEventData _eventData)
-		{
+	{
+		audioSource.clip = onDragSound;
 		audioSource.Play();
 		
-			if(originalParent == null){}
+		if(originalParent == null){}
 
-			originalParent = transform.parent;
-			if(canvas == null)
-			{
-				canvas = gameObject.GetComponentInParent<Canvas>();
-			}
-			
-			transform.SetParent(canvas.transform, true);
-			transform.SetAsLastSibling();
-
-			Dragging = true;
-		}
-
-		public void OnDrag(PointerEventData _eventData)
+		originalParent = transform.parent;
+		if(canvas == null)
 		{
-			if(Dragging)
-			{
-				transform.position = _eventData.position;
-			}
+			canvas = gameObject.GetComponentInParent<Canvas>();
 		}
-		private void Update()
+			
+		transform.SetParent(canvas.transform, true);
+		transform.SetAsLastSibling();
+
+		Dragging = true;
+	}
+
+	public void OnDrag(PointerEventData _eventData)
+	{
+		if(Dragging)
 		{
-			Vector2 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			transform.position = _eventData.position;
 		}
+	}
+	private void Update()
+	{
+		Vector2 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
 
-		public void OnEndDrag(PointerEventData _eventData)
+	public void OnEndDrag(PointerEventData _eventData)
+	{
+		Dragging = false;
+		transform.SetParent(originalParent);
+		transform.localPosition = Vector3.zero;
+		// List<RaycastResult> results = new();
+
+		// EventSystem.current.RaycastAll(_eventData, results);
+
+		// foreach(RaycastResult result in results)
+		// {
+		// 	if(!result.gameObject.CompareTag("Road"))
+		// 	{
+		// 		Instantiate(towerToSpawn, result., quaternion.identity);
+		// 		Object.Destroy(this.gameObject);
+		// 	}
+		// 		return;
+		// }
+
+			
+		RaycastHit hit = new();
+		Vector2 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			
+		Instantiate(towerToSpawn,vec , quaternion.identity);
+
+		audioSource.Stop();
+		
+		audioSource.clip = onPlaceSound;
+		audioSource.Play();
+
+		if(towerButton)
 		{
-			Dragging = false;
-			transform.SetParent(originalParent);
-			transform.localPosition = Vector3.zero;
-			// List<RaycastResult> results = new();
-
-			// EventSystem.current.RaycastAll(_eventData, results);
-
-			// foreach(RaycastResult result in results)
-			// {
-			// 	if(!result.gameObject.CompareTag("Road"))
-			// 	{
-			// 		Instantiate(towerToSpawn, result., quaternion.identity);
-			// 		Object.Destroy(this.gameObject);
-			// 	}
-			// 		return;
-			// }
-
-			
-			RaycastHit hit = new();
-			Vector2 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			
-			Instantiate(towerToSpawn,vec , quaternion.identity);
-			if(towerButton)
-			{
-				Destroy(towerButton);
-			}
-			Object.Destroy(this.gameObject);
-			return;
-				
-			
+			Destroy(towerButton);
 		}
-
-
+		
+		Object.Destroy(this.gameObject);
+		return;		
+	}
 }
